e84de1c88ab21e454cecb5ca9f7d5f3665430018
Pramudiliyanage <pramudi.liyanage@techventuras.com>
2024-09-04 14:50:55 +0530

    first commit

---
+++ ui/src/components/AddTaskForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import axios from "axios";
+import { API_URL } from "../utils";
+
+function AddTaskForm(fetchTasks) {
+  const [newTask, setNewTask] = useState("");
+
+  const addNewTask = async () => {
+    try {
+      await axios.post(API_URL, {
+        name: newTask,
+        completed: false,
+      });
+
+      await fetchTasks();
+
+      setNewTask("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <Typography align="center" variant="h2" paddingTop={2} paddingBottom={2}>
+        My Task List
+      </Typography>
+      <div className="addTaskform"></div>
+      <TextField
+        size="small"
+        variant="outlined"
+        label="task"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <Button disabled={!newTask} variant="outlined" onClick={addNewTask}>
+        <AddIcon />
+      </Button>
+    </div>
+  );
+}
+
+export default AddTaskForm;
